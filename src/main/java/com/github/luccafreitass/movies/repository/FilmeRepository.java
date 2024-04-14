2875c2a82c23d5b901f52049918e598bc2ec9bc4
eduardojuhn <dudu.juhn.02@gmail.com>
2024-04-14 02:58:08 -0300

    feat: retorna optional

--- src/main/java/com/github/luccafreitass/movies/repository/FilmeRepository.java
+++ src/main/java/com/github/luccafreitass/movies/repository/FilmeRepository.java
@@ -1,6 +1,7 @@
 package com.github.luccafreitass.movies.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +10,6 @@ import com.github.luccafreitass.movies.model.Filme;
 
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
 	List<Filme> findTop5ByOrderByAvaliacaoDesc();
-	Filme findByTitulo(String titulo);
+	Optional<Filme> findByTitulo(String titulo);
 	FilmeResponseDto findDtoById(Long id);
 }
