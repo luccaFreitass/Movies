85e2a7f64463604d38bf6bfef505328086220607
Lucca Freitas <luccafreitas@Luccas-MacBook-Air.local>
2024-04-16 08:42:49 -0300

    fix:arrumando as classes service e repositorio para o funcionamento do obterporId

--- src/main/java/com/github/luccafreitass/movies/controller/FilmeController.java
+++ src/main/java/com/github/luccafreitass/movies/controller/FilmeController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/filmes")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class FilmeController {
 
 	@Autowired
@@ -25,8 +26,8 @@ public class FilmeController {
 	}
 
 	@GetMapping("/{id}")
-	public FilmeResponseDto obterPorId(@PathVariable Long id){
-		    return service.obterPorId(id);
+	public FilmeResponseDto obterPorId(@PathVariable Long id) {
+		return service.obterPorId(id);
 	}
 
 	@PostMapping
--- src/main/java/com/github/luccafreitass/movies/repository/FilmeRepository.java
+++ src/main/java/com/github/luccafreitass/movies/repository/FilmeRepository.java
@@ -5,11 +5,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.github.luccafreitass.movies.DTO.FilmeResponseDto;
 import com.github.luccafreitass.movies.model.Filme;
 
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
 	List<Filme> findTop5ByOrderByAvaliacaoDesc();
+
 	Optional<Filme> findByTitulo(String titulo);
-	FilmeResponseDto findDtoById(Long id);
+
 }
